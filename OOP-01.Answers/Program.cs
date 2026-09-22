995bdda7eaa3a37ce49f42060b05099646a8b60a
Nour Atef <Nour Atef@LAPTOP-2M74GP4J>
2026-09-22 19:14:23 +0300

    Question 1

--- OOP-01.Answers/Program.cs
+++ OOP-01.Answers/Program.cs
@@ -6,7 +6,22 @@ namespace OOP_01.Answers
     {
         static void Main(string[] args)
         {
+            //a)
+            //class is a reference type while struct is a value type
+            //class supports inheritence while struct cannot inherit
             
+            //b)
+            //Class stores more 
+            //in memory than struct which has max capacity of 16 bytes
+
+
+
+
+
+
+
+
+
 
 
 
