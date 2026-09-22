af47f94d4f3c003742190340c0e0e718459c7f6c
Nour Atef <Nour Atef@LAPTOP-2M74GP4J>
2026-09-22 16:34:55 +0300

    Add project files.

---
+++ OOP-01.Answers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+
+namespace OOP_01.Answers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+        }
+
+
+
+    }
+
+}
